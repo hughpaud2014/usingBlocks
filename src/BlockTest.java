29e0d6947c165825a434d088ec9f208f4c7a73b6
Paul <hughpaud@gmail.com>
2021-01-29 14:43:28 -0600

    Initial commit

---
+++ src/BlockTest.java
@@ -0,0 +1,19 @@
+/*
+Credit: Oracle: Java The Complete Reference Eleventh Edition
+Created by: Paul Hughes
+
+Demonstrate a block of code
+
+Call this file "BlockTest.java"
+ */
+
+public class BlockTest {
+    public static void main(String[] args) {
+        int x, y;
+        y = 20;
+
+        // The target of this loop is a block
+
+    }
+    
+}
