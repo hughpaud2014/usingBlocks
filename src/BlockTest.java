35acafb89f8da1c6bc10f56bfa20e3a7630380b8
Paul <hughpaud@gmail.com>
2021-01-29 15:01:37 -0600

    :books: Sample Block Test

--- src/BlockTest.java
+++ src/BlockTest.java
@@ -13,6 +13,12 @@ public class BlockTest {
         y = 20;
 
         // The target of this loop is a block
+        for (x = 0; x < 10; x++) {
+            System.out.println("This is x: " + x);
+            System.out.println("This is y: " + y);
+            y = y - 2;
+
+        }
 
     }
     
